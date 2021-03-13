c3e5bab06afe424078ce5efaf1cf517354cd8d05
Lou Falla <lou3892@gmail.com>
2021-03-12 22:39:51 -0500

    adding city function and storing cities

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,3 +8,18 @@ var id = "c52ebbd0a3f56c59ee405c95485eb3a0";
 function storeCities() {
     localStorage.setItem("cities", JSON.stringify(cityList));
 }
+// adds last searched city to list-group as button for user to select city
+function createCityList(){
+    $(".cityList").empty();
+    cityList.forEach(function(city) {
+        $(".cityList").prepend($(`<button class="list-group-item list-group-item-action cityButton" data-city="${city}">${city}</button>`));
+    })
+}
+
+// loads cityList from local storage and calls api to get data for last searched city if it exists
+function init() {
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+
+    if (storedCities !== null) {
+        cityList = storedCities;
+    }
