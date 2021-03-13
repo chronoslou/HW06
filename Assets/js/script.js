5b7ace48cc8bb37354718ebdc63bd60c20c4c561
Lou Falla <lou3892@gmail.com>
2021-03-12 22:40:50 -0500

    adding function for UV index

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -23,3 +23,38 @@ function init() {
     if (storedCities !== null) {
         cityList = storedCities;
     }
+
+    createCityList();
+
+    if (cityList) {
+        var thisCity = cityList[cityList.length - 1]
+        getCurrentWeather(thisCity, id);
+        getForecast(thisCity, id);
+    }
+}
+
+// gets current forecast for selected city and calls uv index function
+function getCurrentWeather(thisCity, id) {
+    var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${thisCity}&units=imperial&appid=${id}`;
+    var cityLat;
+    var cityLong;
+
+    $.ajax({
+        url: weatherURL,
+        method: "GET"
+    }).then(function (data) {
+        $(".cityToday").append(
+            `<div class="row ml-1">
+                <h3 class="mr-3">${data.name} (${(new Date(1000 * data.dt).getUTCMonth()) + 1}/${(new Date(1000 * data.dt).getUTCDate()) - 1}/${new Date(1000 * data.dt).getUTCFullYear()})</h3>
+                <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">
+            </div>`
+        )
+        $(".cityToday").append(`<p>Temperature: ${data.main.temp} &degF</p>`)
+        $(".cityToday").append(`<p>Humidity: ${data.main.humidity} %</p>`)
+        $(".cityToday").append(`<p>Wind: ${data.wind.speed} mph</p>`)
+        cityLat = data.coord.lat;
+        cityLong = data.coord.lon;
+        getUVI(id, cityLat, cityLong);
+    })
+
+}
